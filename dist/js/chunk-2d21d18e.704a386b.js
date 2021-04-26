28d078c13606c3d8988c108b70dac464c0b53fab
Tiago Heineck <tiagovda@gmail.com>
2021-04-26 16:35:02 -0300

    build

---
+++ dist/js/chunk-2d21d18e.704a386b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21d18e"],{d09e:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"contato"},[t("h1",[e._v("Ninguém")])])}],u=t("2877"),a={},l=Object(u["a"])(a,c,s,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d21d18e.704a386b.js.map
